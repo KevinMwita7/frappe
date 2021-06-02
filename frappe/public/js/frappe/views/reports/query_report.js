cb21094b2b595d4719957f199cba49c0deec2481
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-06-02 13:37:18 +0300

    Update query_report.js

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -938,7 +938,8 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		this.make_access_log('Print', 'PDF');
 		frappe.render_grid({
 			template: print_settings.columns ? 'print_grid' : custom_format,
-			title: __(this.report_name),
+			// title: __(this.report_name),
+			title: "Testing",
 			subtitle: filters_html,
 			print_settings: print_settings,
 			landscape: landscape,
